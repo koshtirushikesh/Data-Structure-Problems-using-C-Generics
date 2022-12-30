a72e531eeab259a1cf4e8351351a96060f80ace8
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-30 17:20:21 +0530

    simple Linked List

--- Program.cs
+++ Program.cs
@@ -6,19 +6,10 @@
         {
             LinkedList linkedList = new LinkedList();
 
-            //linkedList.Add(70);            //uc1
-            //linkedList.Add(30);
-            //linkedList.Add(56);
 
-
-            //linkedList.AddFirst(70);        //uc2
-            //linkedList.AddFirst(30);
-            //linkedList.AddFirst(56);
-
-            linkedList.AddFirst(56);          //uc3
+            linkedList.AddFirst(56);          
             linkedList.AddFirst(30);
             linkedList.AddFirst(70);
-
 
             linkedList.display();
             
