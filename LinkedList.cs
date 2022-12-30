2cda201b00b7a03e798692dd0553dfb5d0efa301
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-30 09:37:10 +0530

    simple Linked List

---
+++ LinkedList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LInkedListDemo
+{
+    public class LinkedList
+    {
+        public Node head;
+
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while(temp.next!=null)
+                {
+                    temp = temp.next;
+                }temp.next = node;
+            }
+        }
+        public void display()
+        {
+            Node temp = this.head;
+            if(temp ==null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while(temp!=null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LInkedListDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+
+            linkedList.display();
+            
+        }
+    }
+}
